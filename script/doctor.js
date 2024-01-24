4ac41bb9f2dc748103ba1f136b5430f734b30728
ievgen275 <ievgen3435@gmail.com>
2024-01-24 21:36:48 +0200

    fix search and add new doctors

--- script/doctor.js
+++ script/doctor.js
@@ -2,13 +2,17 @@ document.querySelector('.search').oninput = function () {
    let valueInput = document.querySelector('.search').value.toLowerCase();
    let doctors = document.querySelectorAll('.doctor_cart');
 
-   if (valueInput != '') {
+   if (valueInput !== '') {
       doctors.forEach(function (elem) {
-         if (elem.innerText.search(valueInput) == -1) {
+         if (elem.innerText.toLowerCase().search(valueInput) === -1) {
             elem.classList.add('hide');
          } else {
             elem.classList.remove('hide');
-         };
+         }
       });
-   };
+   } else {
+      doctors.forEach(function (elem) {
+         elem.classList.remove('hide');
+      });
+   }
 };
